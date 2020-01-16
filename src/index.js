5dee8a49a8609a4c927943e4d88f53c091b4054c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-16 10:34:20 -0300

    Move page to pages folder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './config/ReactotronConfig';
+import App from './components/pages/index';
 import Style from './styles';
 
 ReactDOM.render(
