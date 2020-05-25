d5ab55fdf867f37b770ce6a0271551c3842435e8
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-25 20:45:06 -0300

    Show map and developers

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
+import Map from 'google-map-react';
 import api from '~/services/api';
 import Register from '~/components/Register';
 import Developer from '~/components/Developer';
@@ -8,46 +9,54 @@ import Layout from '~/components/Layout';
 
 export default () => {
   const [developers, setDevelopers] = useState([]);
+  const [center, setCenter] = useState();
+
+  useEffect(() => {
+    (async () => {
+      await navigator.geolocation.getCurrentPosition(
+        pos => {
+          const { latitude: lat, longitude: lng } = pos.coords;
+          setCenter({ lat, lng });
+        },
+        () => {}
+      );
+    })();
+  }, []);
 
   useEffect(() => {
     (async () => {
       const { data } = await api.get('developers');
-      setDevelopers(data);
+      setDevelopers(
+        data.map(developer => {
+          const [lng, lat] = developer.location.coordinates;
+          return {
+            ...developer,
+            location: {
+              lat,
+              lng,
+            },
+          };
+        })
+      );
     })();
   }, []);
 
-  const handleSubmit = useCallback(
-    async ({ github_username, latitude, longitude, techs }) => {
-      const { data } = await api.post('developers', {
-        github_username,
-        latitude: Number(latitude),
-        longitude: Number(longitude),
-        techs,
-      });
-      setDevelopers([...developers, data]);
-    },
-    [developers]
-  );
-
   return (
     <Layout>
-      <Container>
-        <Aside>
-          <strong>Cadastrar</strong>
-          <Register onSubmit={handleSubmit} />
-        </Aside>
-        <Main>
-          <ul>
-            {developers.map(dev => (
-              <Developer
-                data-testid={`developer_${dev._id}`}
-                key={dev._id}
-                dev={dev}
-              />
-            ))}
-          </ul>
-        </Main>
-      </Container>
+        <Map
+          style={{ height: 'calc(100% - 55px)' }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLEMAP_API_KEY }}
+          defaultCenter={{
+            lat: -23.6821604,
+            lng: -46.8754915,
+          }}
+          center={center}
+          zoom={15}
+        >
+          {developers.map(({ location: { lat, lng }, ...data }) => (
+            <Developer key={data._id} lng={lng} lat={lat} data={data} />
+          ))}
+        </Map>
     </Layout>
   );
 };
