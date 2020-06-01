5c091a7c222564d1780a4112a0996fec7412eca2
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-05-31 21:40:52 -0300

    Create socket service

---
+++ src/services/socket.js
@@ -0,0 +1,26 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio(process.env.REACT_APP_API_URL, {
+  autoConnect: false,
+});
+
+export function connect(latitude, longitude, techs) {
+  socket.io.opts.query = {
+    latitude,
+    longitude,
+    techs,
+  };
+  socket.connect();
+}
+
+export function disconnect() {
+  if (socket.connected) {
+    socket.disconnect();
+  }
+}
+
+export function subscribe(event, callback) {
+  socket.on(event, callback);
+}
+
+export default socket;
