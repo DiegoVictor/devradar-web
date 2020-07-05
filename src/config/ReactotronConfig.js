451cf77eb34bff4e3eafa15bbdd88e653d9653b6
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-04 22:17:22 -0300

    Remove reactotron

--- src/config/ReactotronConfig.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Reactotron from 'reactotron-react-js';
-
-if (process.env.NODE_ENV === 'development') {
-  Reactotron.configure()
-    .connect()
-    .clear();
-}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '~/config/ReactotronConfig';
 import Dashboard from '~/pages/Dashboard';
 
 ReactDOM.render(
