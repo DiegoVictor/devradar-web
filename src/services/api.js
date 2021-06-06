6c1200e9a5d1f9bc11116e6cd498bb0b01c80483
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-06 15:38:31 -0300

    Read api url environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/${process.env.REACT_APP_API_VERSION}`,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export function setAuthorization(value) {
