502cb4988f6763ccddc4a4bea2feb26858a8403d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-12 13:52:14 -0300

    Return validation errors messages

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -119,11 +119,19 @@ export default () => {
       try {
         formRef.current.setErrors({});
 
+        if (!processing) {
           break;
         }
-      }
+        }
       } catch (err) {
+        const validationErrors = {};
+        if (err instanceof Yup.ValidationError) {
+          err.inner.forEach(error => {
+            validationErrors[error.path] = error.message;
+          });
 
+          formRef.current.setErrors(validationErrors);
+        }
       }
     },
     [action, params, processing, updateStateAndStorage]
