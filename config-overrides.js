d86835f83bc3d953f793abc4070289f179c0045b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-15 19:28:55 -0300

    Read jest configurations

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { addBabelPlugin, override } = require('customize-cra');
+const jestConfig = require('./jest.config.js');
 
-module.exports = override(
-  addBabelPlugin([
-    'babel-plugin-root-import',
-    {
-      rootPathSuffix: 'src',
-    },
-  ])
-);
+module.exports = {
+  webpack: override(
+    addBabelPlugin([
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ])
+  ),
+  jest: config => {
+    return { ...config, ...jestConfig };
+  },
+};
