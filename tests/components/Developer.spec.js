b8fbea145056c39d6d2f5e5a38b3966ce87fc4a6
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-16 18:55:23 -0300

    Move to component tests page

---
+++ tests/components/Developer.spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Developer from '~/components/Developer';
+import factory from '../utils/factory';
+
+describe('Developer component', () => {
+  it('should be able to show a developer data', async () => {
+    const dev = await factory.attrs('Developer');
+    const { getByAltText, getByText, getByTestId } = render(
+      <Developer data={dev} />
+    );
+
+    expect(getByAltText(dev.name)).toHaveProperty('src', dev.avatar_url);
+    expect(getByText(dev.name)).toBeInTheDocument();
+    expect(getByText(dev.techs.join(', '))).toBeInTheDocument();
+    expect(getByTestId(`profile_${dev._id}`)).toHaveAttribute(
+      'href',
+      `https://github.com/${dev.github_username}`
+    );
+  });
+
+  it('should be able to show a developer github username', async () => {
+    const dev = await factory.attrs('Developer');
+    delete dev.name;
+
+    const { getByAltText, getByText, getByTestId } = render(
+      <Developer data={dev} />
+    );
+
+    expect(getByAltText(dev.github_username)).toHaveProperty(
+      'src',
+      dev.avatar_url
+    );
+    expect(getByText(dev.github_username)).toBeInTheDocument();
+    expect(getByText(dev.techs.join(', '))).toBeInTheDocument();
+    expect(getByTestId(`profile_${dev._id}`)).toHaveAttribute(
+      'href',
+      `https://github.com/${dev.github_username}`
+    );
+  });
+});
