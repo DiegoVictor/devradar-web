8a8e68548920e42aaa8066a7416a75bcda0a3d8e
Diego Victor <diegovictorgonzaga@gmail.com>
2023-08-06 00:41:20 -0300

    Fix import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 
-import Dashboard from './pages/Dashboard';
+import { Dashboard } from './pages/Dashboard';
 import Theme from './styles/theme';
 
 export default () => (
